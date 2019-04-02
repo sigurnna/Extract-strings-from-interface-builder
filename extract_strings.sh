ca367fa58b2d3c855e6c8932a68d04492b0f6829
seungjun.lee <sigurnna@gmail.com>
2019-04-02 23:39:33 +0900

    remove hard coding...

--- extract_strings.sh
+++ extract_strings.sh
@@ -1,25 +1,47 @@
 #!/usr/bash
 
-PROJECT_DIR="/Users/iseungjun/Development/upbit-ios-global"
-SAVE_PATH="$PROJECT_DIR/extract_string"
-SAVE_FILENAME="extract.strings"
+help() {
+    echo "extract_strings_from_interface_builder: [-p <xcode_project_path>] [-e <strings_extract_path>]"
+    exit 1
+}
+
+while getopts "p:e:h" opt; do
+    case $opt in
+        p) # project dir
+            PROJECT_DIR="$OPTARG"
+            ;;
+        e) # extract dir
+            EXTRACT_DIR="$OPTARG"
+            ;;
+        h) help ;;
+    esac
+done
+
+if [ -z "$PROJECT_DIR" ] || [ -z "$EXTRACT_DIR" ]; then
+    help
+    exit 1
+fi
+
+EXTRACT_FILENAME="extract.strings"
 
 cd $PROJECT_DIR
 
-if [ ! -d "$SAVE_PATH" ]; then
-    mkdir $SAVE_PATH
+if [ ! -d "$EXTRACT_DIR" ]; then
+    mkdir $EXTRACT_DIR
 fi
 
 # Extract strings from storyboards or xibs
 find . -name "*.storyboard" -o -name "*.xib" | while read FILENAME; do
     ibtool --generate-strings-file "$FILENAME.txt" $FILENAME
-    mv "$FILENAME.txt" $SAVE_PATH
+    mv "$FILENAME.txt" $EXTRACT_DIR
 done
 
-cd $SAVE_PATH
+cd $EXTRACT_DIR
 
 # Concatenate string files to single file
 find . -name "*.txt" | while read FILENAME; do
-    cat $FILENAME >> $SAVE_FILENAME
+    cat $FILENAME >> $EXTRACT_FILENAME
     rm $FILENAME
 done
+
+exit 0
